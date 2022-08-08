2235297e0089dfb5531fe715a1e321af2dc3413f
Mauro Manuel <MAURO.MANUEL@UNITEL.CO.AO>
2022-08-08 07:42:23 +0100

    feat: Project created

---
+++ rhclient/src/main/java/com/masprogtechs/rhclient/RhclientApplication.java
@@ -0,0 +1,13 @@
+package com.masprogtechs.rhclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhclientApplication.class, args);
+	}
+
+}
---
+++ rhclient/src/test/java/com/masprogtechs/rhclient/RhclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masprogtechs.rhclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
